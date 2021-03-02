34b4e929557d7d4fda847131d46ecefeabc58afe
yuelimin <yueliminvc@outlook.com>
2021-03-02 21:09:35 +0800

    修复部分已知错误.

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ export function getList(params) {
   return request({
     url: `/transaction/projects/q?pageNo=${params.pageNo}&pageSize=${params.pageSize}`,
     method: 'post',
-    params
+    // params
   })
 }
 // 管理员审核标的信息
@@ -13,7 +13,7 @@ export function loanVerify(params) {
   return request({
     url: `/transaction/m/projects/${params.id}/projectStatus/${params.approveStatus}?commission=0`,
     method: 'put',
-    params
+    // params
   })
 }
 // 满标审核
@@ -21,6 +21,6 @@ export function lendVerify(params) {
   return request({
     url: `/transaction/m/loans/${params.id}/projectStatus/${params.approveStatus}?commission=0`,
     method: 'put',
-    params
+    // params
   })
 }
